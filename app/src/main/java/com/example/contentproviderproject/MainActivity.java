1b7298a9d789455cbad1f63877cf3e45c9fc723f
Hesham <hmostafa3bd@gmail.com>
2019-05-24 22:47:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/contentproviderproject/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.contentproviderproject;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.UriMatcher;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txv;
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    private static final String CONTACT_NAME = "myName";
+    private static final String CONTACT_AGE = "myAge";
+    private static final String AUTHORITY = "com.guinness.own.PROVIDER";
+    private static final String BASE_PATH = "contacts";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+    // Constant to identify the requested operation
+    private static final int CONTACTS = 1;
+    private static final int CONTACT_ID = 2;
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        uriMatcher.addURI(AUTHORITY,BASE_PATH, CONTACTS);
+        uriMatcher.addURI(AUTHORITY,BASE_PATH + "/#",CONTACT_ID);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txv = (TextView) findViewById(R.id.textDBInfo);
+        FetchContact();
+
+
+    }
+
+    private  void FetchContact(){
+        ArrayList<String> Contacts = new ArrayList<>();
+        String[] projection    = null;
+        String selection = null;
+        String[] selectionArgs = null;
+        String sortOrder= null;
+        ContentResolver resolver =getContentResolver();
+        Cursor cursor = resolver.query(CONTENT_URI,projection,selection,selectionArgs,sortOrder);
+        while (cursor.moveToNext()){
+            String Name =cursor.getString(cursor.getColumnIndex(CONTACT_NAME));
+            Integer Number =cursor.getInt(cursor.getColumnIndex(CONTACT_AGE));
+            Log.d("guinness","Number : "+Number +"  " +"Name:"+ Name);
+            Contacts.add("Name: "+Name +"\n"+"Number :" + Number);
+        }
+        txv.setText(Contacts.toString()+"\n ");
+
+
+    }
+}
