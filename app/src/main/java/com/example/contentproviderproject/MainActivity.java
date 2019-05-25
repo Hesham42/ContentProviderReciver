57cc44a39e76aa40b35437f8ce541143df3c2093
hesham <hmostafa3bd@gmail.com>
2019-05-25 06:36:09 +0200

    LOOP NOTIFICATION

--- app/src/main/java/com/example/contentproviderproject/MainActivity.java
+++ app/src/main/java/com/example/contentproviderproject/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.contentproviderproject;
 
 import android.Manifest;
+import android.app.LoaderManager;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.ContentResolver;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
 import android.content.UriMatcher;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,26 +30,77 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    TextView txv;
+import static com.example.contentproviderproject.App.CHANEL_1_ID;
+
+public class MainActivity extends AppCompatActivity  implements LoaderManager.LoaderCallbacks<Cursor>{
+    private NotificationManagerCompat managerCompat;
+    private CursorAdapter cursorAdapter;
+    ArrayList<ModelPerson> Contacts;
+    boolean  flag = false;
     private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
     private static final String CONTACT_NAME = "myName";
     private static final String CONTACT_AGE = "myAge";
+    public static final String CONTACT_IDS = "_id";
+
     private static final String AUTHORITY = "com.guinness.own.PROVIDER";
     private static final String BASE_PATH = "contacts";
     public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+
+
+    // Constant to identify the requested operation
+    private static final int CONTACTS = 1;
+    private static final int CONTACT_ID = 2;
+
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        uriMatcher.addURI(AUTHORITY,BASE_PATH, CONTACTS);
+        uriMatcher.addURI(AUTHORITY,BASE_PATH + "/#",CONTACT_ID);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txv = (TextView) findViewById(R.id.textDBInfo);
-        FetchContact();
+        managerCompat = NotificationManagerCompat.from(this);
+        Contacts = new ArrayList<ModelPerson>();
+        cursorAdapter = new ContactsCursorAdapter(this,null,0);
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+//        AddNotification(FetchContact(),flag);
+        getLoaderManager().initLoader(0, null, this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                restartLoader();
 
+            }
+        });
+    }
+
+
+    private void restartLoader() {
+        getLoaderManager().restartLoader(0,null,this);
+    }
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this,CONTENT_URI,null,null,null,CONTACT_IDS+"DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        cursorAdapter.swapCursor(data);
 
     }
 
-    private  void FetchContact(){
-        ArrayList<String> Contacts = new ArrayList<>();
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
+
+    }
+    
+    private  ArrayList<ModelPerson>  FetchContact(){
         String[] projection    = null;
         String selection = null;
         String[] selectionArgs = null;
@@ -48,9 +110,45 @@ public class MainActivity extends AppCompatActivity {
         while (cursor.moveToNext()){
             String Name =cursor.getString(cursor.getColumnIndex(CONTACT_NAME));
             Integer Number =cursor.getInt(cursor.getColumnIndex(CONTACT_AGE));
-            Log.d("guinness","Number : "+Number +"  " +"Name:"+ Name);
-            Contacts.add("Name: "+Name +"\n"+"Number :" + Number);
+            Integer Id= cursor.getInt(
+                    cursor.getColumnIndex(CONTACT_IDS));
+            ModelPerson person= new ModelPerson(Name,Number,Id);
+             Contacts.add(person);
         }
-        txv.setText(Contacts.toString()+"\n ");
+        return Contacts;
+
     }
-}
+
+    private void AddNotification(ArrayList<ModelPerson> contacts ,boolean b) {
+        if (b ==false){
+            int size = Contacts.size();
+            if (size > 0) {
+                for (int i = 0; i < Contacts.size(); i++) {
+                    if (i==0){
+                        flag=true;
+                        Intent activeIntent = new Intent(this,MainActivity.class);
+                        PendingIntent pendingIntent = PendingIntent.getActivity(
+                                this,
+                                0,
+                                activeIntent,
+                                0);
+
+                        Notification notification =  new
+                                NotificationCompat.Builder(this,CHANEL_1_ID)
+                                .setSmallIcon(R.drawable.notifaction_24dp)
+                                .setContentTitle(Contacts.get(i).getCONTACT_NAME())
+                                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                                .setContentIntent(pendingIntent)
+                                .build();
+                        managerCompat.notify(Contacts.get(i).getCONTACT_IDS(),notification);
+
+
+                    }
+                }
+            }
+        }
+
+    }
+
+    }
